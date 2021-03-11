625f1dbf4b6d1ff447b7dc97cd053bff1360fc5f
Peter Leonov <gojpeg@gmail.com>
2021-03-11 18:15:12 +0100

    save

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,29 +1,23 @@
 // easy to derive as all the canonization happens in the constructor
 #[derive(Debug, PartialEq, Eq)]
-pub struct Clock {
-    hours: i32,
-    minutes: i32,
-}
+pub struct Clock(i32);
+
+const HOUR: i32 = 60;
+const DAY: i32 = 24 * HOUR;
 
 impl Clock {
-    pub fn new(hours: i32, minutes: i32) -> Self {
-        // the goal was to keep everything immutable
-        let hours = (hours + minutes / 60) % 24;
-        let minutes = minutes % 60;
-        let (minutes, hours) = if minutes < 0 {
-            (minutes + 60, hours - 1)
-        } else {
-            (minutes, hours)
-        };
-        let hours = if hours < 0 { hours + 24 } else { hours };
-        Self { hours, minutes }
+    pub fn new(h: i32, m: i32) -> Self {
+        // Wrapping and handling negatives here
+        // to get the simple derivable binary Eq.
+        let m = (m + h * HOUR) % DAY;
+        Self(if m < 0 { m + DAY } else { m })
     }
 
     pub fn to_string(&self) -> String {
-        format!("{:02}:{:02}", self.hours, self.minutes)
+        format!("{:02}:{:02}", self.0 / HOUR, self.0 % HOUR)
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        Self::new(self.hours, self.minutes + minutes)
+        Self::new(0, self.0 + minutes)
     }
 }
