c196c169a4cd4b2604e5503fe5f6ae1a9b63bbbf
Peter Leonov <gojpeg@gmail.com>
2021-03-12 15:36:47 +0100

    save

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 // easy to derive as all the canonization happens in the constructor
 #[derive(Debug, PartialEq, Eq)]
 pub struct Clock(i32);
@@ -9,15 +11,16 @@ impl Clock {
     pub fn new(h: i32, m: i32) -> Self {
         // Wrapping and handling negatives here
         // to get the simple derivable binary Eq.
-        let m = (m + h * HOUR) % DAY;
-        Self(if m < 0 { m + DAY } else { m })
-    }
-
-    pub fn to_string(&self) -> String {
-        format!("{:02}:{:02}", self.0 / HOUR, self.0 % HOUR)
+        Self((m + h * HOUR).rem_euclid(DAY))
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
         Self::new(0, self.0 + minutes)
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:02}:{:02}", self.0 / HOUR, self.0 % HOUR)
     }
 }
