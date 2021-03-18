f7991db17c532c634b330ea252f8a7e2d46cc32d
Peter Leonov <gojpeg@gmail.com>
2021-03-18 16:30:16 +0100

    save

--- sieve/src/lib.rs
+++ sieve/src/lib.rs
@@ -1,3 +1,16 @@
+// Expected to have no multiplication or division.
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
-    unimplemented!("Construct a vector of all primes up to {}", upper_bound);
+    let mut primes = Vec::with_capacity(upper_bound as usize + 1 >> 8);
+    let mut is_prime = vec![true; upper_bound as usize + 1];
+    for n in 2..=upper_bound {
+        if is_prime[n as usize] {
+            primes.push(n);
+        }
+
+        for m in (n..=upper_bound).step_by(n as usize) {
+            is_prime[m as usize] = false;
+        }
+    }
+
+    primes
 }
