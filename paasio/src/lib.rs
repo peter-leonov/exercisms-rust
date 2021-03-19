46f522a6960c93096fdd1183a4446f69fa049335
Peter Leonov <gojpeg@gmail.com>
2021-03-19 19:35:28 +0100

    save

---
+++ paasio/src/lib.rs
@@ -0,0 +1,85 @@
+use std::io::{Read, Result, Write};
+
+pub struct ReadStats<R> {
+    wrapped: R,
+    reads: usize,
+    bytes_through: usize,
+}
+
+impl<R: Read> ReadStats<R> {
+    // _wrapped is ignored because R is not bounded on Debug or Display and therefore
+    // can't be passed through format!(). For actual implementation you will likely
+    // wish to remove the leading underscore so the variable is not ignored.
+    pub fn new(wrapped: R) -> ReadStats<R> {
+        ReadStats {
+            wrapped,
+            reads: 0,
+            bytes_through: 0,
+        }
+    }
+
+    pub fn get_ref(&self) -> &R {
+        &self.wrapped
+    }
+
+    pub fn bytes_through(&self) -> usize {
+        self.bytes_through
+    }
+
+    pub fn reads(&self) -> usize {
+        self.reads
+    }
+}
+
+impl<R: Read> Read for ReadStats<R> {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+        let bytes = self.wrapped.read(buf)?;
+        self.bytes_through += bytes;
+        self.reads += 1;
+        Ok(bytes)
+    }
+}
+
+pub struct WriteStats<W> {
+    wrapped: W,
+    writes: usize,
+    bytes_through: usize,
+}
+
+impl<W: Write> WriteStats<W> {
+    // _wrapped is ignored because W is not bounded on Debug or Display and therefore
+    // can't be passed through format!(). For actual implementation you will likely
+    // wish to remove the leading underscore so the variable is not ignored.
+    pub fn new(wrapped: W) -> WriteStats<W> {
+        WriteStats {
+            wrapped,
+            writes: 0,
+            bytes_through: 0,
+        }
+    }
+
+    pub fn get_ref(&self) -> &W {
+        &self.wrapped
+    }
+
+    pub fn bytes_through(&self) -> usize {
+        self.bytes_through
+    }
+
+    pub fn writes(&self) -> usize {
+        self.writes
+    }
+}
+
+impl<W: Write> Write for WriteStats<W> {
+    fn write(&mut self, buf: &[u8]) -> Result<usize> {
+        let bytes = self.wrapped.write(buf)?;
+        self.bytes_through += bytes;
+        self.writes += 1;
+        Ok(bytes)
+    }
+
+    fn flush(&mut self) -> Result<()> {
+        self.wrapped.flush()
+    }
+}
