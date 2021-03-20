706de0d67500783444183336dfa6ad59b231530a
Peter Leonov <gojpeg@gmail.com>
2021-03-20 20:37:13 +0100

    save

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -1,7 +1,8 @@
 // Deliberetely trying not to use explicit pointers,
 // using slices and some fancy ptr arith instead.
-pub fn find<T: Eq + Ord>(array: &[T], key: T) -> Option<usize> {
-    let mut slice = array;
+pub fn find<T: PartialOrd, A: AsRef<[T]>>(array: A, key: T) -> Option<usize> {
+    let array = array.as_ref();
+    let mut slice = array.as_ref();
 
     while !slice.is_empty() {
         let mid = slice.len() / 2;
@@ -11,9 +12,9 @@ pub fn find<T: Eq + Ord>(array: &[T], key: T) -> Option<usize> {
         } else if key > *x {
             &slice[mid + 1..]
         } else {
-            let start = array.as_ptr() as usize;
+            let start = &array[0] as *const T as usize;
             let found = &slice[mid] as *const T as usize;
-            let size = std::mem::size_of::<i32>();
+            let size = std::mem::size_of::<T>();
             return Some((found - start) / size);
         }
     }
