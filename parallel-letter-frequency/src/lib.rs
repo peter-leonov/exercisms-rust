9cb6f11bd0914b42cf1e72023bcf09403204bf7c
Peter Leonov <gojpeg@gmail.com>
2021-03-30 13:10:55 +0200

    save

--- parallel-letter-frequency/src/lib.rs
+++ parallel-letter-frequency/src/lib.rs
@@ -1,29 +1,32 @@
+use crossbeam::thread;
 use std::collections::HashMap;
-use std::sync::Arc;
-use std::thread;
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    // naive clone everything
-    let input = input.join("");
-
-    let result = thread::spawn(|| {
-        let mut char_count = HashMap::with_capacity(256);
-        for line in &[input] {
-            // try flat_map
-            for c in line.chars() {
-                // did not check if this is actually more efficient
-                // than lowercasing the whole string at once,
-                // but I hope Rust is smart enough to do the iterator
-                // overhead elimination magic here for me :)
-                if c.is_alphabetic() {
-                    for lc in c.to_lowercase() {
-                        let count = char_count.entry(lc).or_insert(0);
-                        *count += 1;
+    thread::scope(|s| {
+        let result = s
+            .spawn(|_| {
+                let mut char_count = HashMap::with_capacity(256);
+                for line in input {
+                    // try flat_map
+                    for c in line.chars() {
+                        // did not check if this is actually more efficient
+                        // than lowercasing the whole string at once,
+                        // but I hope Rust is smart enough to do the iterator
+                        // overhead elimination magic here for me :)
+                        if c.is_alphabetic() {
+                            for lc in c.to_lowercase() {
+                                let count = char_count.entry(lc).or_insert(0);
+                                *count += 1;
+                            }
+                        }
                     }
                 }
-            }
-        }
-        char_count
-    });
-    result.join().unwrap()
+                char_count
+            })
+            .join()
+            .unwrap();
+
+        result
+    })
+    .unwrap()
 }
