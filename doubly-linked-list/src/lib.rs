1ed56c36a886d121d3fda14bec365a9f29b84269
Peter Leonov <gojpeg@gmail.com>
2021-04-01 21:47:58 +0200

    save

---
+++ doubly-linked-list/src/lib.rs
@@ -0,0 +1,136 @@
+// this module adds some functionality based on the required implementations
+// here like: `LinkedList::pop_back` or `Clone for LinkedList<T>`
+// You are free to use anything in it, but it's mainly for the test framework.
+mod pre_implemented;
+
+struct Node<T> {
+    value: T,
+    next: Link<T>,
+    prev: Link<T>,
+}
+
+type Link<T> = Option<Box<Node<T>>>;
+type Ref<'a, T> = Option<&'a mut Box<Node<T>>>;
+
+pub struct LinkedList<T> {
+    head: Link<T>,
+    back: Link<T>,
+}
+
+pub struct Cursor<'a, T> {
+    // list: &'a LinkedList<T>,
+    current: Ref<'a, T>,
+}
+
+pub struct Iter<'a, T>(&'a Link<T>);
+
+impl<T> Node<T> {
+    pub fn boxed(value: T) -> Box<Node<T>> {
+        Box::new(Self {
+            value,
+            next: None,
+            prev: None,
+        })
+    }
+}
+
+impl<'a, T> LinkedList<T> {
+    pub fn new() -> Self {
+        Self {
+            head: None,
+            back: None,
+        }
+    }
+
+    // You may be wondering why it's necessary to have is_empty()
+    // when it can easily be determined from len().
+    // It's good custom to have both because len() can be expensive for some types,
+    // whereas is_empty() is almost always cheap.
+    // (Also ask yourself whether len() is expensive for LinkedList)
+    pub fn is_empty(&self) -> bool {
+        self.head.is_none()
+    }
+
+    pub fn len(&self) -> usize {
+        self.iter().count()
+    }
+
+    /// Return a cursor positioned on the front element
+    pub fn cursor_front(&'a mut self) -> Cursor<'a, T> {
+        unimplemented!()
+        // Cursor {
+        //     list: self,
+        //     current: self.head.as_mut(),
+        // }
+    }
+    /// Return a cursor positioned on the back element
+    pub fn cursor_back(&'a mut self) -> Cursor<'a, T> {
+        Cursor {
+            // list: self,
+            current: self.back.as_mut(),
+        }
+    }
+
+    /// Return an iterator that moves from front to back
+    pub fn iter(&'a self) -> Iter<'a, T> {
+        Iter(&self.head)
+    }
+}
+
+// the cursor is expected to act as if it is at the position of an element
+// and it also has to work with and be able to insert into an empty list.
+impl<T> Cursor<'_, T> {
+    /// Take a mutable reference to the current element
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        unimplemented!()
+    }
+
+    /// Move one position forward (towards the back) and
+    /// return a reference to the new position
+    #[allow(clippy::should_implement_trait)]
+    pub fn next(&mut self) -> Option<&mut T> {
+        if let Some(node) = self.current.take() {
+            self.current = node.next.as_mut();
+        };
+        self.current.as_mut().map(|node| &mut node.value)
+    }
+
+    /// Move one position backward (towards the front) and
+    /// return a reference to the new position
+    pub fn prev(&mut self) -> Option<&mut T> {
+        unimplemented!()
+    }
+
+    /// Remove and return the element at the current position and move the cursor
+    /// to the neighboring element that's closest to the back. This can be
+    /// either the next or previous position.
+    pub fn take(&mut self) -> Option<T> {
+        unimplemented!()
+    }
+
+    pub fn insert_after(&mut self, element: T) {
+        match self.current {
+            Some(ref mut node) => {
+                node.next = Some(Node::boxed(element));
+            }
+            None => (),
+        }
+    }
+
+    pub fn insert_before(&mut self, _element: T) {
+        unimplemented!()
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<&'a T> {
+        if let Some(node) = self.0 {
+            self.0 = &node.next;
+            Some(&node.value)
+        } else {
+            None
+        }
+    }
+}
